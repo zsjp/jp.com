fcfb612158810fe7703421623ec1a52aace23863
zsjp <crazyzhanyue@126.com>
2016-12-29 17:12:17 +0800

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,19 @@
+function skin1()
+{
+	var oL=document.getElementById('theme');
+	
+	oL.href='css/style.css';
+}
+
+function skin2()
+{
+	var oL=document.getElementById('theme');
+	
+	oL.href='css/css2.css';
+}
+function skin3()
+{
+	var oL=document.getElementById('theme');
+	
+	oL.href='css/css3.css';
+}
